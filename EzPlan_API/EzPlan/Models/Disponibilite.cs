41b5d356a50eee5b0f7a1b7e112484df42ceacd6
Guillaume <onemademu@gmail.com>
2021-12-07 16:44:45 -0500

    Ajout du corps des classes Utilisateur, HoraireDisponibilites et Disponibilite

--- EzPlan_API/EzPlan/Models/Disponibilite.cs
+++ EzPlan_API/EzPlan/Models/Disponibilite.cs
@@ -7,5 +7,22 @@ namespace EzPlan.Models
 {
     public class Disponibilite
     {
+        public int DisponibiliteID { get; set; }
+        public string Journee { get; set; }
+        public double HeureDebut { get; set; }
+        public double HeureFin { get; set; }
+        public HoraireDisponibilites HoraireDisponibilites { get; set; }
+
+        public Disponibilite()
+        {
+        }
+
+        public Disponibilite(HoraireDisponibilites horaireDisponibilites, string journee, double heureDebut, double heureFin)
+        {
+            HoraireDisponibilites = horaireDisponibilites;
+            Journee = journee;
+            HeureDebut = heureDebut;
+            HeureFin = heureFin;
+        }
     }
 }
--- EzPlan_API/EzPlan/Models/HoraireDisponibilites.cs
+++ EzPlan_API/EzPlan/Models/HoraireDisponibilites.cs
@@ -7,5 +7,29 @@ namespace EzPlan.Models
 {
     public class HoraireDisponibilites
     {
+        public int HoraireDisponibilitesID { get; set; }
+        public string Nom { get; set; }
+        public List<Disponibilite> Disponibilites { get; set; }
+        public Utilisateur Utilisateur { get; set; }
+
+        public HoraireDisponibilites()
+        {
+        }
+
+        public HoraireDisponibilites(string nom, Utilisateur utilisateur)
+        {
+            Utilisateur = utilisateur;
+            Nom = nom;
+        }
+
+        private string AjouterDisponibilite(Disponibilite disponibilite)
+        {
+            if (!this.Disponibilites.Contains(disponibilite))
+            {
+                this.Disponibilites.Add(disponibilite);
+                return "Ajouté avec succès.";
+            }
+            return "Cette disponibilite existe déjà.";
+        }
     }
 }
