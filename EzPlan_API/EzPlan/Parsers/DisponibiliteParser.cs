9786012bbe2c0889e6b8c3ae64f5f7d11d3756c6
JyBastien <77074544+JyBastien@users.noreply.github.com>
2021-12-09 17:18:12 -0500

    stash

--- EzPlan_API/EzPlan/Parsers/DisponibiliteParser.cs
+++ EzPlan_API/EzPlan/Parsers/DisponibiliteParser.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using EzPlan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EzPlan.Parsers
 {
     public class DisponibiliteParser
     {
-    }
+        public static Disponibilite ParseFromJSON(string disponibiliteJSON)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Disponibilite>(disponibiliteJSON);
+        }
+        public static JsonResult ParseToJSON(Disponibilite disponibilite)
+        {
+            var retour = new
+            {
+                disponibilite.DisponibiliteID,
+                disponibilite.Journee,
+                disponibilite.HeureDebut,
+                disponibilite.HeureFin
+            };
+            return new JsonResult(retour);
+        }
+}
 }
