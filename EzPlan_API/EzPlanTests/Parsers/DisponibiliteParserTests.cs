65fd0f71ffbb4a069639e194a50fa8707f77e34f
JyBastien <77074544+JyBastien@users.noreply.github.com>
2021-12-09 17:17:40 -0500

    stashing

---
+++ EzPlan_API/EzPlanTests/Parsers/DisponibiliteParserTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EzPlan.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EzPlan.Models;
+
+namespace EzPlan.Parsers.Tests
+{
+    [TestClass()]
+    public class DisponibiliteParserTests
+    {
+        [TestMethod()]
+        public void ParseToJSONTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
