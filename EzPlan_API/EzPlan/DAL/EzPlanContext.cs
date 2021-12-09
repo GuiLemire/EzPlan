ba46f4ab7cb8a5ae63bc89a606635c09925efa8d
Guillaume <onemademu@gmail.com>
2021-12-09 14:14:04 -0500

    Creeated DbContext

--- EzPlan_API/EzPlan/DAL/EzPlanContext.cs
+++ EzPlan_API/EzPlan/DAL/EzPlanContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EzPlan.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -8,9 +9,26 @@ namespace EzPlan.DAL
 {
     public class EzPlanContext : DbContext
     {
-        public EzPlanContext() : base("EzPlanContext")
-        {
+        public DbSet<Disponibilite> Disponibilites { get; set; }
+        public DbSet<HoraireDisponibilites> HorairesDisponibilites { get; set; }
+        public DbSet<JourneePlanifiee> JourneesPlanifiees { get; set; }
+        public DbSet<PartieDeTachePlanifiee> PartiesDeTachesPlanifiees { get; set; }
+        public DbSet<SemainePlanifiee> SemainesPlanifiees { get; set; }
+        public DbSet<Tache> Taches { get; set; }
+        public DbSet<TachePlanifiee> TachesPlanifiees { get; set; }
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
 
+        //Se fait seul et utilise le nom des DbSet, mais permet d'éviter les "s" dans le nom des tables
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Disponibilite>().ToTable("Disponibilite");
+            modelBuilder.Entity<HoraireDisponibilites>().ToTable("HoraireDisponibilites");
+            modelBuilder.Entity<JourneePlanifiee>().ToTable("JourneePlanifiee");
+            modelBuilder.Entity<PartieDeTachePlanifiee>().ToTable("PartieDeTachePlanifiee");
+            modelBuilder.Entity<SemainePlanifiee>().ToTable("SemainePlanifiee");
+            modelBuilder.Entity<Tache>().ToTable("Tache");
+            modelBuilder.Entity<TachePlanifiee>().ToTable("TachePlanifiee");
+            modelBuilder.Entity<Utilisateur>().ToTable("Utilisateur");
         }
     }
 }
